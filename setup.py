9b5cc9bc4ff23cb7412cb46b18e6760f4ab99943
kjnapes <kjnapier@umich.edu>
2020-11-05 12:27:27 -0500

    pushing basic functions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='skyplot',
+   version='0.0.1',
+   description='Plot sky maps.',
+   author='Kevin Napier',
+   author_email='kjnapier@umich.edu',
+   url="https://github.com/kjnapes/skyplot",
+   packages=['skyplot']
+)
